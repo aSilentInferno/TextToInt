c6c28585f8a9adb9f51739058e3e6ac5236cc64d
aSilentInferno <aSilentInferno@outlook.com>
2025-01-29 14:32:09 +0100

    Add unit tests for NumeralsToInteger class to validate numeral conversion functionality

--- TextToInt.Test/NumeralsToIntegerTest.cs
+++ TextToInt.Test/NumeralsToIntegerTest.cs
@@ -1,5 +1,58 @@
+using TextToInt;
+
 namespace TextToInt.Test;
     
-    public class NumeralsToIntegerTest
+public class NumeralsToIntegerTest
+{
+
+    [Fact]
+    public void Convert_SingleNumeral()
     {
+        // Arrange
+        string input = "five";
+        int expected = 5;
+        
+        // Act
+        int output = NumeralsToInteger.Convert(input);
+        // Assert
+        Assert.Equal(output, expected);
     }
+
+    [Fact]
+    public void Convert_WithHpyhen()
+    {
+        // Arrange
+        string input = "twenty-five";
+        int expected = 25;
+        
+        // Act
+        int output = NumeralsToInteger.Convert(input);
+        // Assert
+        Assert.Equal(output, expected);
+    }
+    [Fact]
+    public void Convet_BiggerNumber()
+    {
+        // Arrange
+        string input = "five hundred twenty-five";
+        int expected = 525;
+
+        // Act
+        int output = NumeralsToInteger.Convert(input);
+        // Assert
+        Assert.Equal(output, expected);
+    }
+    [Fact]
+    public void Convert_InvalidNumeral()
+    {
+        // Arrange
+        string input = "meow";
+        int? expected = null;
+
+        // Act
+        int output = NumeralsToInteger.Convert(input);
+        // Assert
+        Assert.Equal(output, expected);
+    }
+   
+}
